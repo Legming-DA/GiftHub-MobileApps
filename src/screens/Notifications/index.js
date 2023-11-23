da633c959cc4263c9838e7ed7ba1f6f5f6a9013d
Legming-DA <legmingdwi@gmail.com>
2023-11-23 20:09:30 +0700

    [BAB 6]-Animation

--- src/screens/Notifications/index.js
+++ src/screens/Notifications/index.js
@@ -1,54 +1,97 @@
-import React, { useState } from "react";
-import { ScrollView, Text, StyleSheet, Image, View, TouchableOpacity } from "react-native";
-import {
-    ArrowLeft, Scan,
-} from "iconsax-react-native";
+import React, { useState, useRef } from "react";
+import { ScrollView, Text, StyleSheet, View, Animated } from "react-native";
+import { ArrowLeft, Scan, } from "iconsax-react-native";
 import { fontType, colors } from "../../theme";
 import { useNavigation } from "@react-navigation/native";
 
+const Notif = () => {
+        return (
+            <View style={styles.card}>
+                <View style={styles.cardIcon}>
+                    <Text style={styles.subheading}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </Text>
+                </View>
+            </View>
+        )
+}
 
 export default function Notifications() {
     const navigation = useNavigation()
+    const scrollY = useRef(new Animated.Value(0)).current;
+    const diffClampY = Animated.diffClamp(scrollY, 0, 52);
+    const headerY = diffClampY.interpolate({
+        inputRange: [0, 52],
+        outputRange: [0, -52],
+    });
     return (
         <View style={styles.container}>
-            <View style={styles.Headers}>
+            <Animated.View style={[styles.Headers, { transform: [{ translateY: headerY }] }]}>
                 <ArrowLeft color={colors.white()} varian="linear" size={25} />
-            </View>
-            <View style={styles.card}>
-                <View style={styles.cardIcon}>
-                    <Text style={styles.subheading}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    </Text>
+                <Text style={{ alignItems: "center", color: colors.white() }}>Notifications</Text>
+            </Animated.View>
+            <Animated.ScrollView showsVerticalScrollIndicator={false}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                    { useNativeDriver: true },
+                )}>
+                <View style={styles.card}>
+                    <View style={styles.cardIcon}>
+                        <Text style={styles.subheading}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's
+                        </Text>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.card}>
-                <View style={styles.cardIcon}>
-                    <Text style={styles.subheading}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's
-                    </Text>
+                <View style={styles.card}>
+                    <View style={styles.cardIcon}>
+                        <Text style={styles.subheading}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </Text>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.card}>
-                <View style={styles.cardIcon}>
-                    <Text style={styles.subheading}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    </Text>
+                <View style={styles.card}>
+                    <View style={styles.cardIcon}>
+                        <Text style={styles.subheading}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </Text>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.card}>
-                <View style={styles.cardIcon}>
-                    <Text style={styles.subheading}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    </Text>
+                <View style={styles.card}>
+                    <View style={styles.cardIcon}>
+                        <Text style={styles.subheading}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's
+                        </Text>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.card}>
-                <View style={styles.cardIcon}>
-                    <Text style={styles.subheading}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's
-                    </Text>
+                <View style={styles.card}>
+                    <View style={styles.cardIcon}>
+                        <Text style={styles.subheading}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's
+                        </Text>
+                    </View>
                 </View>
-            </View>
+                <View style={styles.card}>
+                    <View style={styles.cardIcon}>
+                        <Text style={styles.subheading}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.card}>
+                    <View style={styles.cardIcon}>
+                        <Text style={styles.subheading}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.card}>
+                    <View style={styles.cardIcon}>
+                        <Text style={styles.subheading}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's
+                        </Text>
+                    </View>
+                </View>
+            </Animated.ScrollView>
         </View>
     );
 }
